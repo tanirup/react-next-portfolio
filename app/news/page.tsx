ada5f68134e19a9d953a4ea692e5657bd4930468
aoto0218 <ktc24a31g0010@edu.kyoto-tech.ac.jp>
2025-01-25 13:57:17 +0900

    編集

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -5,6 +5,8 @@ import SearchField from "../_components/SearchField";
 import { NEWS_LIST_LIMIT } from "../_constants";
 import NewsList from "@/app/_components/NewsList";
 
+export const revalidate = 0;
+
 export default async function Page() {
     const { contents: news, totalCount } = await getNewsList({
         limit: NEWS_LIST_LIMIT,
